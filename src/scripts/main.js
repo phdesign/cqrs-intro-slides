731e3da391574fb2da32c148db2a6747ca0cd322
Paul Heasley <paul.heasley@gmail.com>
2015-05-21 17:11:23 +1000

    Refreshed from slush generator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,11 @@
 // Bespoke.js
 bespoke.from('article', [
-  bespoke.themes.cube(),
-  bespoke.plugins.keys(),
-  bespoke.plugins.touch(),
-  bespoke.plugins.bullets('li, .bullet'),
-  bespoke.plugins.backdrop(),
-  bespoke.plugins.scale(),
-  bespoke.plugins.hash(),
-  bespoke.plugins.progress()
+    bespoke.themes.cube(),
+    bespoke.plugins.bullets('li, .bullet'),
+    bespoke.plugins.backdrop(),
+    bespoke.plugins.scale(),
+    bespoke.plugins.hash(),
+    bespoke.plugins.progress(),
+    bespoke.plugins.keys(),   
+    bespoke.plugins.touch()
 ]);
